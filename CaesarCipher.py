1a45ea217066c4c6a1fb086d3005c300ff370fb0
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-06-03 22:55:52 +0100

    decrypt code

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -19,6 +19,23 @@ def encrypt(text, shift):
     print(f"The encoded text is {cipher_text}") 
 
 encrypt(text, shift)
+
+def decrypt(text, shift):
+    decipher_text = ""
+    for letter in text:
+        position = alphabet.index(letter)   #index() method
+        #print(letter, position)
+        new_position = position - shift
+        new_letter =  alphabet[new_position]
+        #print(new_position, new_letter)
+        decipher_text += new_letter
+
+      
+    print(f"The decoded text is {decipher_text}") 
+
+encrypt(text, shift)
+
+decrypt(text, shift)
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
@@ -34,3 +51,16 @@ encrypt(text, shift)
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+#decrpt
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
